2550d3fa429e32ea9cda63a9f6bca0025e1e5c8c
Alex Anderson <alex.anderson@ku.edu>
2022-11-02 18:22:05 -0500

    got rid of exec

--- tokens.d
+++
@@ -1,9 +0,0 @@
-tokens.o: tokens.cpp tokens.hpp position.hpp grammar.hh ast.hpp \
- symbol_table.hpp types.hpp errors.hpp
-tokens.hpp:
-position.hpp:
-grammar.hh:
-ast.hpp:
-symbol_table.hpp:
-types.hpp:
-errors.hpp:
